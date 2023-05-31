0ae2cec3952008046eb4097a8d1d49e306626698
Nelsson José <njose29@outlook.com>
2023-05-31 10:05:05 -0500

    feat(setting): Adding setting mapping profile

---
+++ Columbia.Code/Domain/Mapping/SettingProfile.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using $safesolutionname$.Dto.Setting;
+
+namespace $safesolutionname$.Domain.Mapping
+{
+    public class SettingProfile : Profile
+    {
+        public SettingProfile()
+        {
+            CreateMap<Entity.Setting, SettingDto>()
+                .ReverseMap();
+
+            CreateMap<Entity.Setting, UpdateSettingDto>()
+                .ReverseMap();
+
+            CreateMap<Entity.Setting, GetSettingDto>()
+                .ReverseMap();
+
+            CreateMap<Entity.Setting, SearchSettingDto>()
+                .ReverseMap();
+        }
+    }
+}
