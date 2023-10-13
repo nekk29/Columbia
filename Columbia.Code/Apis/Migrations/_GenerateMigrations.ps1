217cba16fa1b22b6984810622ed04b06b61a0680
Nelsson José <njose29@outlook.com>
2023-10-12 23:11:43 -0500

    feat(migrations): Adding environment to migrations

--- Columbia.Code/Apis/Migrations/_GenerateMigrations.ps1
+++ Columbia.Code/Apis/Migrations/_GenerateMigrations.ps1
@@ -3,4 +3,4 @@ dotnet new tool-manifest
 dotnet tool install dotnet-ef --version 6.0.8
 
 dotnet ef migrations add CoreMigration_InitialMigration --context CoreDbContext
-dotnet ef database update --context CoreDbContext
+dotnet ef database update --context CoreDbContext -- --environment {{environment}}
